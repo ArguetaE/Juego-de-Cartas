91ac66515a1487e803f4ed2a7ea1580a4b4c2c1b
Jose Argueta <josearguetaespinoza@gmail.com>
2024-10-19 22:36:45 -0600

    Mini juego de cartas

---
+++ assets/js/juego.js
@@ -0,0 +1,59 @@
+let deck = [];
+const tipos = ['C','D','H','S'];
+const especiales = ['A','J','Q','K'];
+
+const crearDesk = () => {
+    for (let i = 2; i < 10; i++) {
+        for (let tipo of tipos) {
+            deck.push(i+tipo);
+        }
+    }
+
+    for (let tipo of tipos) {
+        for (const esp of especiales) {
+            deck.push(esp+tipo)
+        }
+    }
+
+    console.log(deck);
+    deck = _.shuffle( deck );
+    console.log( deck );
+}
+
+crearDesk();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const crearDesk = () =>{
+//     for (let i = 2; i <= 10; i++) {
+//         for (let tipo of tipos) {
+//             deck.push(i + tipo);
+//         } 
+//     }
+//     console.log(deck);
+// }
+// crearDesk();
